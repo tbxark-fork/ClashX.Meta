76132f41bc6055ac4b40269da9cf5a193ca767b4
mrFq1 <1xxbx0il0@mozmail.com>
2026-08-23 19:59:52 +0800

    feat: wire toolbar filters into logs and connections

--- ClashX/Dashboard/Views/ContentTabs/Connections/ConnectionsView.swift
+++ ClashX/Dashboard/Views/ContentTabs/Connections/ConnectionsView.swift
@@ -10,29 +10,24 @@ struct ConnectionsView: View {
 	
 	@EnvironmentObject var data: ClashConnsStorage
 	
+	@EnvironmentObject var toolbarState: DashboardToolbarState
 	@State private var searchString: String = ""
 	
-    var body: some View {
+	    var body: some View {
 
-		ConnectionsTableView(data: data.conns,
-							 filterString: searchString)
-		.background(Color(compatible: .textBackgroundColor))
-			.onAppear {
-				guard let s = ToolbarStore.shared.searchStrings["conns"] else { return }
-				searchString = s
-				NotificationCenter.default.post(name: .initSearchString, object: nil, userInfo: ["string": s])
-			}
-			.onDisappear {
-				ToolbarStore.shared.searchStrings["conns"] = searchString
-			}
-			.onReceive(NotificationCenter.default.publisher(for: .toolbarSearchString)) {
-				guard let string = $0.userInfo?["String"] as? String else { return }
-				searchString = string
-			}
-			.onReceive(NotificationCenter.default.publisher(for: .stopConns)) { _ in
-				stopConns()
-			}
-	}
+			ConnectionsTableView(data: data.conns,
+								 filterString: searchString)
+			.background(Color(compatible: .textBackgroundColor))
+				.onAppear {
+					searchString = toolbarState.searchText
+				}
+				.onChange(of: toolbarState.searchText) { newValue in
+					searchString = newValue
+				}
+				.onReceive(NotificationCenter.default.publisher(for: .stopConns)) { _ in
+					stopConns()
+				}
+		}
 	
 	func stopConns() {
 		Task {
